52fd3863c59f79c1d14b0993bc0e07ca27d3f420
Dorukium <dorukersoytwitch@gmail.com>
2025-07-29 00:11:32 +0300

    Add AppVeyor CI configuration for automated builds

---
+++ copy-system-icons.bat
@@ -0,0 +1,5 @@
+@echo off 
+echo Sistem iconlari kopyalaniyor... 
+copy "C:\Windows\System32\shell32.dll,0" LMWallpaper.ico 
+copy "C:\Windows\System32\shell32.dll,0" small.ico 
+echo Sistem iconlari kopyalandi. 
