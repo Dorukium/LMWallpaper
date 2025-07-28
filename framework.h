e29b7cc7286ebbd7db9c1b1ecc1a8d683e51097e
Dorukium <dorukersoytwitch@gmail.com>
2025-07-29 00:19:55 +0300

    Add missing framework.h and targetvar.h files, fix CMakeLists.txt

---
+++ framework.h
@@ -0,0 +1,86 @@
+// framework.h
+#pragma once
+
+#include "Headers/targetvar.h"
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <shellapi.h>
+#include <dwmapi.h>
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+#include <commdlg.h>
+#include <commctrl.h>
+#include <psapi.h>
+#include <dshow.h>
+#include <evr.h>
+#include <mfapi.h>
+#include <mfidl.h>
+#include <mfreadwrite.h>
+#include <mferror.h>
+#include <gdiplus.h>
+
+// C++ Standard Library
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <deque>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <filesystem>
+#include <functional>
+#include <memory>
+#include <algorithm>
+
+// COM için gerekli
+#include <comdef.h>
+#include <initguid.h>
+
+// Link kütüphaneleri
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
+#pragma comment(lib, "dwmapi.lib")
+#pragma comment(lib, "psapi.lib")
+#pragma comment(lib, "strmiids.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "oleaut32.lib")
+#pragma comment(lib, "uuid.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "windowscodecs.lib")
+#pragma comment(lib, "mf.lib")
+#pragma comment(lib, "mfplat.lib")
+#pragma comment(lib, "mfreadwrite.lib")
+#pragma comment(lib, "mfuuid.lib")
+#pragma comment(lib, "gdiplus.lib")
+
+// Namespace kullanımı
+using namespace Gdiplus;
+
+// Global değişkenler
+extern HINSTANCE hInst;
+extern HWND hWndMain;
+
+// GDI+ için başlatma/kapatma fonksiyonları
+inline void InitializeGDIPlus() {
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+}
+
+inline void ShutdownGDIPlus() {
+    ULONG_PTR gdiplusToken = 0;
+    GdiplusShutdown(gdiplusToken);
+}
